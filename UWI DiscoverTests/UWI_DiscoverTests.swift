439f6ff4b82bb069a7437071db12f9b2d6d5adf1
Javon Davis <javonldavis14@gmail.com>
2016-01-22 15:08:45 -0500

    Initial Commit

---
+++ UWI DiscoverTests/UWI_DiscoverTests.swift
@@ -0,0 +1,36 @@
+//
+//  UWI_DiscoverTests.swift
+//  UWI DiscoverTests
+//
+//  Created by Javon Davis on 22/01/2016.
+//  Copyright © 2016 Javon Davis. All rights reserved.
+//
+
+import XCTest
+@testable import UWI_Discover
+
+class UWI_DiscoverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
